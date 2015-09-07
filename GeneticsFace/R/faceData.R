6a5728fad10c8cf3ace97f4dd730e2eaef0c7b86
Stefan Boehringer <github@s-boehringer.org>
2015-09-07 17:41:20 +0200

    initial commit

---
+++ GeneticsFace/R/faceData.R
@@ -0,0 +1,94 @@
+#
+#	faceData.R
+#Thu Sep  3 14:33:52 2015
+
+#
+#	<p> visigen file format
+#
+
+readCoordinateFile_visigen = function(path) {
+	readTable(sprintf('[SEP=T,HEADER=F,NAMES=node;x;y]:%s', path));
+}
+
+#
+#	<p> ini xml files
+#
+
+#
+# adapted form stackoverflow
+#
+
+require('XML');
+list2xmlRaw = function(node, l) {
+	for(i in seq_along(l)) {
+		child = newXMLNode(names(l)[i], parent = node);
+		if (typeof(l[[i]]) == "list") list2xmlRaw(child, l[[i]]) else xmlValue(child) = l[[i]];
+	} 
+}
+list2xml = function(l, rootName = 'root', asDocument = FALSE) {
+	document = if (asDocument) newXMLDoc() else NULL;
+	root = newXMLNode(rootName, doc = document);
+	list2xmlRaw(root, l);
+	r = if (asDocument) document else root;
+	r
+}
+
+xmlTemplateModelGraph = list(
+	GraphType = 'MODEL_GRAPH',
+	NumberOfNodes = NA,
+	NumberOfEdges = 0,
+	Trafo = list(
+		TrafoType = 'GABOR',
+		NumberOfLevels = 5,
+		NumberOfDirections = 8,
+		Maximum = 1.5708,
+		Factor = 0.707107,
+		Sigma = 6.28319,
+		DcFree = 1,
+		SpaceAccuracy = 3
+	),
+	#Topology = list(Edge = list(SourceNode = 1, TargetNode = 2))
+	Topology = list(),
+	#EdgeList = list(Edge = list(Index = 1, EdgeInfoType = 'MODEL_EDGE_INFO'))
+	EdgeList = list()
+);
+
+#' @par: graph array containing coordinates
+coordinateStructure_ini = function(graph, rootName = 'ModelGraph', asDocument = TRUE) {
+	graphStructureL = xmlTemplateModelGraph;
+	graphL = ilapply(apply(graph, 1, as.list), function(coords, i) {
+		list(
+			Index = i - 1, ToBeUsed = 1, NodeInfoType = 'MODEL_NODE_INFO',
+			Position = list(Element = avu(coords[1]), Element = avu(coords[2]))
+		)
+	});
+	names(graphL) = rep('Node', length(graphL));
+	graphStructureL$NumberOfNodes = nrow(graph);
+	graphStructureL$NodeList = graphL;
+	list2xml(graphStructureL, rootName = rootName, asDocument = asDocument);
+}
+
+writeCoordinateFile_ini = function(graph, output = NULL) {
+	header = '<?xml version="1.0"?>';
+	coordinateStructure_ini(graph);
+	saveXML(ini, output);
+}
+
+readCoordinateFile_ini = function(path, input) {
+	xml = xmlParseDoc(file);
+	coords = as.integer(
+		xpathSApply(xml,'//ModelGraph/NodeList/Node/Position/Element', function(i)xmlValue(i))
+	);
+}
+
+prepareAveraging_ini = function(pathImg, output,
+	pathGraph = Sprintf('%{base}s.xml', base = splitPath(pathImg)$fullbase) {
+
+	if (file.info(output)$isdir) output = Sprintf('%{output}s/%{base}s', base = splitPath(pathImg)$base);
+	cmd = Sprintf(con('ini_X64 --extract-jets ',
+		'--input-graph %{pathGraph}Q ',
+		'--input-image %{pathImg}Q.tif ',
+		'--output-graph %{output}Q'));
+	Log(cmd, 2);
+	System(cmd, 2);
+}
---
+++ GeneticsFace/R/facePreprocess.R
@@ -0,0 +1,274 @@
+#
+#	facePreprocess.R
+#Mon Aug 31 12:18:52 CEST 2015
+
+convertImages = function(path, output, extension = 'jpg', quality = 80L) {
+	files = list_files_with_exts(path, extension);
+	r = lapply(files, function(path){
+		file = splitPath(path)$file;
+		cmd = Sprintf("convert -quality %{quality}s %{path}Q %{output}Q/%{file}Q");
+		System(cmd, 3);
+	});
+	r
+}
+
+listImages = function(path, extension = 'jpg')list_files_with_exts(path, extension);
+readImages = function(path, extension, ...)lapply(listImages(path, extension), function(file)readImage(file));
+images2grey = function(is)lapply(is, function(i)channel(i, 'gray'))
+
+readCoordinatesFromFile = function(path, path2metaRegex = NULL, reader = readCoordinateFile_visigen) {
+	d0 = reader(path);
+	if (!('id' %in% names(d0))) {
+		name = splitPath(path)$base;
+		d0$id = name;
+	}
+	if (is.null(path2metaRegex)) {
+		d0$type = NA;
+	} else {
+		d0$type = as.character(fetchRegexpr(regex, name, captures = T));
+		if (length(type) == 0) type = NA;
+	}
+	d0
+}
+
+#' @arg path Path to folder from which files with extension `pos` are parsed
+#' @arg path2metaRegex Provide regular expression that is applied to the path and caputres of which
+#'   are used as meta-information returned in the type column of the result data frame
+readCoordinateData = function(path, path2metaRegex = NULL, reader = readCoordinateFile_visigen) {
+	files = list_files_with_exts(path, 'pos');
+	r0 = lapply(files, readCoordinatesFromFile, reader = reader, path2metaRegex = path2metaRegex);
+	d = do.call(rbind, r0);
+	d = data.frame.types(d, factor = c('id', 'type', 'node'));
+	d
+}
+
+
+table2array = function(d, id = 'id', coords = c('x', 'y'), node = 'node') {
+	ids = levels(d[[id]]);	# individuals
+	nodes = as.character(d[[node]])[1:(nrow(d)/length(ids))];
+	# expected order of nodes
+	nodeRows = rep(rep.each(nodes, length(coords)), length(ids));
+	d1 = reshape.long(d, coords);
+	if (any(d1$node != nodeRows)) stop('order of nodes inconsistent bewteen individuals');
+	print(nodes);
+	a = array(d1$value, c(length(coords), length(nodes), length(ids)));
+	a = aperm(a, c(2, 1, 3));
+	a
+}
+
+array2table = function(a, ids, nodes, coords = c('x', 'y')) {
+	Nr = dim(a)[3];
+
+	dfs = lapply(1:Nr, function(i) {
+		dfCoords = data.frame.types(a[,,i], names = coords);
+		d1 = reshape.wide(data.frame(id = ids[i], node = nodes, dfCoords), ids = 'id', vars = 'node',
+			blockVars = T, reverseNames = T);
+	});
+	d1 = rbindDataFrames(dfs);
+	d1
+}
+
+
+coords2distances = function(d, vertices, patterns = c('%s.x', '%s.y')) {
+	# <p> find columns
+	ns = names(d);
+	coords = nlapply(vertices, function(n) {
+		which.indeces(c(sprintf(patterns[1], n), sprintf(patterns[2], n)), ns)
+	});
+
+	# <A> identify based on x-coordinate, assume y-coordinate to exist
+	modelList = list(x = vertices, y = vertices);
+	r = iterateModels(modelList, .constraint = function(x, y)(x < y), function(i, x, y) {
+		#Log(sprintf('Vertices (%s, %s)', x, y), 4);
+		dims = length(coords[[x]]);
+		dist = sapply(1:dims, function(dim)((d[, coords[[x]][dim]] - d[, coords[[y]][dim]])^2));
+		distEuclidian = apply(dist, 1, function(r)sqrt(sum(r)));
+		distEuclidian
+	}, lapply__ = lapply);
+	distanceNames = apply(r$models_symbolic, 1, function(r)sprintf('d_%s', join(r, sep = '_')));
+	distances = data.frame.types(sapply(r$results, identity), names = distanceNames);
+	distances
+}
+
+#
+#	<p> procrustes
+#
+
+performProcrustes = function(a) {
+	pa = procGPA(a, affine = T);
+	#dr = pa$rotated;	# rotated data
+	pa
+}
+
+procrustesTransformations = function(pa) {
+}
+
+#
+#	<p> transformations
+#
+
+# euclidian norm
+vector.norm = function(v)sqrt(v %*% v)
+# homogeneous coordinates
+hom = function(d)cbind(d, 1)
+# homogeneous coordinates -> euclidian coordinates
+homI = function(d)d[, 1:2]
+
+# X: data matrix
+# Xt: transformed data
+# X' T = Xr' (priming -> homogenous coordinates) => T = X'^- Xr'
+affineBetween = function(X, Xt) {
+	ginv(hom(X)) %*% hom(Xt)
+}
+
+# http://math.stackexchange.com/questions/237369/given-this-transformation-matrix-how-do-i-decompose-it-into-translation-rotati
+# http://math.stackexchange.com/questions/13150/extracting-rotation-scale-values-from-2d-transformation-matrix/13165#13165
+# @result list to be read as follows: rot -> scale -> translate or scale -> rot -> translate
+
+affine2components = function(m) {
+	m = t(m);
+	sx = vector.norm(m[1, 1:2]) * sign(m[1, 1]);
+	sy = vector.norm(m[2, 1:2]) * sign(m[2, 2]);
+	phi = atan2(-m[1, 2], m[1, 1]);
+	phi1 = atan2(m[2, 1], m[2, 2]);
+	aff = list(translate = m[1:2, 3], scale = c(sx, sy), rot = c(phi, phi1));
+	aff
+}
+components2affine = function(aff) with(aff, {
+	m = matrix(c(
+		scale[1] * cos(rot[1]), -scale[1] * sin(rot[1]), translate[1],
+		scale[2] * sin(rot[1]), scale[2] * cos(rot[1]), translate[2],
+		0, 0, 1
+	), nrow = 3, ncol = 3, byrow = T);
+	t(m)
+})
+affineCreate = function(scale = c(1, 1), rot = c(0, 0), translate = c(0, 0)) {
+	components2affine(list(translate = translate, scale = scale, rot = rot));
+}
+
+affRotate = function(data, aff) with(aff, {
+	m = matrix(c(
+		cos(rot[1]), -sin(rot[1]),
+		sin(rot[1]), cos(rot[1])
+	), nrow = 2, ncol = 2, byrow = T);
+	data %*% t(m)
+	
+})
+
+affScale = function(data, aff) with(aff, {
+	m = matrix(c(
+		scale[1], 0,
+		0, scale[2]
+	), nrow = 2, ncol = 2, byrow = T);
+	data %*% t(m)
+})
+
+affTranslate = function(data, aff) with(aff, {
+	cbind(data[, 1, drop = F] + translate[1], data[, 2, drop = F] + translate[2])
+})
+
+# same as data %*% components2affine(aff)
+affPerform = function(data, aff) {
+	affTranslate(affScale(affRotate(data, aff), aff), aff);
+}
+
+
+#
+#	GraphicsMagick
+#
+
+pictureDimensions = function(path) {
+	cmd = Sprintf("identify -format '%%w %%h' %{path}Q");
+	as.integer(splitString(' ', System(cmd, 5, return.output = T)$output))
+}
+
+#
+#	transform image to reflect procrustes rotation
+#	assumption: graph coordinates in pixels
+#	apply transform -> centered coordinates
+#	add scaling to for target coordinates
+#	add translation to center to target coordinates
+#	compute borders to result in a square image
+#	generate imagemagick/graphicsmagick command
+#
+
+pictureMap2output = function(aff, outputDim, scale = 1) {
+	# rescale to fit target size
+	aff = aff %*% affineCreate(scale = rep(scale, 2));
+	aff[3, 1:2] = aff[3, 1:2] + outputDim/2;
+	aff
+}
+
+#' @arg recenter: assume aff is centered around the origin, recenter to middle coordinate of the picture
+pictureTransform = function(path, aff, outputDir, outputDim = pictureDimensions(path),
+	extraArgs = '-type Grayscale', recenter = T, scale = 1, quality = .9) {
+	Dir.create(outputDir);
+	file = splitPath(path)$file;
+
+	aff = pictureMap2output(aff, outputDim, scale);
+	a = sapply(as.vector(t(aff[, 1:2])), function(v)Sprintf('%{v}.2f'));
+	aS = join(a, ',');
+	draw = Sprintf("affine %{aS}s image over 0,0 0,0 %{path}Q");
+	cmd = Sprintf('convert -background grey -size %{w}sx%{h}s xc:snow3 -draw %{draw}Q %{extraArgs}s -quality %{q}.0f %{outputDir}Q/%{file}Q',
+		w = outputDim[1], h = outputDim[2], q = 1e2*quality);
+	System(cmd, 2);
+}
+
+pictureAnnotate = function(path, graph, outputDir, scale = 1, borders = rep(0, 4)) {
+	Dir.create(outputDir);
+	file = splitPath(path)$file;
+	graphS = paste(
+		c('', apply(graph, 1, function(r)join(
+				c(join(sprintf('%.0f', r), ','), join(sprintf('%.0f', r + 2), ','))
+			)
+		))
+	, collapse = ' circle ');
+	cmd = Sprintf('convert %{path}Q -draw %{graphS}Q %{outputDir}Q/%{file}Q');
+	System(cmd, 2);
+}
+
+#
+#	<p> align batch of pictures to common image positions
+#
+
+
+boundingBox = function(a) {
+	list(
+		xlim = c(min(a[, 1, ]), max(a[, 1, ])),
+		ylim = c(min(a[, 2, ]), max(a[, 2, ]))
+	)
+}
+maxAbs = function(e, ...)max(abs(e), ...)
+ext = function(e)(e[2] - e[1])
+scaleFromBoundingBox = function(bb, dimTarget = rep(512, 2), margin = .05) {
+	dimMax = sapply(bb, maxAbs) * 2;
+	scaleD = (dimTarget * (1 - margin)) / dimMax;
+	# rescale to uniformly fit into target size
+	scale = min(scaleD);
+	scale
+}
+
+procrustesTransformImages = function(path, outputDirPrefix = con(path, '_conv'),
+	readCoordinates = readCoordinateData_visigen,
+	annotate = T, dimTarget = rep(512, 2), margin = .05) {
+
+	d = readCoordinates(path);
+	a = table2array(d);
+	pa = performProcrustes(a);
+	scale = scaleFromBoundingBox(boundingBox(pa$rotated), dimTarget, margin);
+
+	ip = listImages(path);
+	if (annotate) pictureAnnotate(ip[1], a[, , 1], 'data/picturesAnnot');
+	aff = affineBetween(a[, , 1], pa$rotated[, , 1]);
+	rot = affine2components(aff);
+	#rot$rot[1] = 20/360 * (2 * pi);
+	aff = components2affine(rot);
+	pictureTransform(ip[1], aff, 'data/picturesProc', outputDim = dimTarget, scale = scale);
+	pdim = pictureDimensions(ip[1]);
+
+	affImg = pictureMap2output(aff, dimTarget, scale);
+	paCoords = homI(hom(a[,,1]) %*% affImg);
+	pictureAnnotate(Sprintf('data/picturesProc/%{file}Q', file = splitPath(ip[1])$file),
+		paCoords, 'data/picturesProcAnnot');
+	
+}
